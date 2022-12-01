e82c81bf7cd223a67e8e3385fa6f91dcf1039996
afnan-khan <mafnank1@gmail.com>
2022-12-01 13:18:11 +0500

    Apply Drop down and Gesture

--- BankMisr/BankMisr/View Controller/ConvertViewController.swift
+++ BankMisr/BankMisr/View Controller/ConvertViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 import RxSwift
 import RxCocoa
-
+import DropDown
+import RxGesture
 
 class CurrencyConverterViewController: UIViewController {
     
     //MARK: - IBOutlet
-    @IBOutlet weak var fromButton :     UIButton!
+    @IBOutlet weak var fromCurrencyView : UIView!
     @IBOutlet weak var toButton :       UIButton!
     @IBOutlet weak var swapButton :     UIButton!
     @IBOutlet weak var fromTextField :  UITextField!
@@ -22,6 +23,11 @@ class CurrencyConverterViewController: UIViewController {
     private var currencyConverterViewModel: CurrencyConverterViewModel!
     
     let disposeBag = DisposeBag()
+    let fromDropDown = DropDown()
+    let toDropDown = DropDown()
+
+    var fromCurrencyId = 1
+    var toCurrencyId = 2
     
     
     //MARK: - View controller life cycle
@@ -31,10 +37,12 @@ class CurrencyConverterViewController: UIViewController {
         // Do any additional setup after loading the view.
         self.currencyConverterViewModel =  CurrencyConverterViewModel()
         
+        setCurrencyDropDown()
+        
         fromTextField.rx.controlEvent([.editingChanged])
             .asObservable().subscribe({ [unowned self] _ in
                 if !(self.fromTextField.text.isNilOrEmpty()) {
-                    let answer = self.currencyConverterViewModel.convertToCurrency(fromCurrency: 1, toCurrency: 2 , value : Double(self.fromTextField.text!)!)
+                    let answer = self.currencyConverterViewModel.convertToCurrency(fromCurrency: fromCurrencyId, toCurrency: toCurrencyId , value : Double(self.fromTextField.text!)!)
                     self.toTextField.text = "\(answer.round(to: 2))"
                 }
             }).disposed(by: disposeBag)
@@ -42,10 +50,33 @@ class CurrencyConverterViewController: UIViewController {
         toTextField.rx.controlEvent([.editingChanged])
             .asObservable().subscribe({ [unowned self] _ in
                 if !(self.toTextField.text.isNilOrEmpty()) {
-                    let answer = self.currencyConverterViewModel.convertToCurrency(fromCurrency: 2, toCurrency: 1 , value : Double(self.toTextField.text!)!)
+                    let answer = self.currencyConverterViewModel.convertToCurrency(fromCurrency: toCurrencyId, toCurrency: fromCurrencyId , value : Double(self.toTextField.text!)!)
                     self.fromTextField.text = "\(answer.round(to: 2))"
                 }
             }).disposed(by: disposeBag)
         
     }
+    
+    private func setCurrencyDropDown() {
+        
+        fromDropDown.anchorView = fromCurrencyView // UIView or UIBarButtonItem
+        fromDropDown.dataSource = currencyConverterViewModel.getCurrencyListName()
+        fromDropDown.selectionAction = { [weak self] (index: Int, item: String) in
+            guard let self = self else { return }
+            self.fromCurrencyId = self.currencyConverterViewModel.getCurrencyId(fromName: item)
+        }
+        
+        fromCurrencyView.rx.tapGesture()
+            .when(.recognized) // This is important!
+            .subscribe(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                self.fromDropDown.show()
+            })
+            .disposed(by: disposeBag)
+        
+    }
+    
+    //MARK: - Actions
+    
+    
 }
--- BankMisr/BankMisr/View Model/CurrencyConverterViewModel.swift
+++ BankMisr/BankMisr/View Model/CurrencyConverterViewModel.swift
@@ -12,15 +12,19 @@ class CurrencyConverterViewModel : NSObject {
     private var currencyModelArray = [CurrencyModel]()
     
     override init() {
-            super.init()
+        super.init()
         initilzeCurrencyModel()
     }
-
-    func initilzeCurrencyModel(){
+    
+    private func initilzeCurrencyModel(){
         
-        currencyModelArray.append(CurrencyModel(currencyId: 1, name: "pkr" , value: 0.1))
-        currencyModelArray.append(CurrencyModel(currencyId: 2,name: "ep" , value: 0.2))
-        currencyModelArray.append(CurrencyModel(currencyId: 3,name: "eu" , value: 4.0))
+        currencyModelArray.append(CurrencyModel(currencyId: 1, name: "Pakistani Rupees" , value: 0.1))
+        currencyModelArray.append(CurrencyModel(currencyId: 2,name: "egyptian pound" , value: 0.2))
+        currencyModelArray.append(CurrencyModel(currencyId: 3,name: "USA Dollar" , value: 4.0))
+    }
+    
+    func getCurrencyListName() -> [String] {
+        return currencyModelArray.map({$0.name})
     }
     
     func convertToCurrency(fromCurrency : Int , toCurrency : Int , value : Double ) -> Double {
@@ -28,10 +32,13 @@ class CurrencyConverterViewModel : NSObject {
         guard let fromCurrencyModel = currencyModelArray.filter({$0.currencyId == fromCurrency}).first else { return 0.0}
         guard let toCurrencyModel = currencyModelArray.filter({$0.currencyId == toCurrency}).first else { return 0.0}
         let answer = (toCurrencyModel.value *  value ) / Double(fromCurrencyModel.value )
-        print("To and From currency model \(answer)")
         return answer
-        
     }
     
+    
+    func getCurrencyId(fromName name : String) -> Int {
+       return  currencyModelArray.filter({$0.name == name}).first!.currencyId
+        
+    }
     
 }
